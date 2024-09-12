047b1ac29628ace188df27c01098dd182f7ec4bf
JoaoVictorDevMeta <joaovictorgouveiar@gmail.com>
2024-09-12 18:45:49 -0300

    updating horarios

--- data/data.js
+++ data/data.js
@@ -6,4 +6,11 @@ export const reps = [
       "Este projeto foi realizado em conjunto com a turma de Informática II 2024. O objetivo era contemplar a nota final da disciplina com um projeto de matemática para o 6º Ano. ",
     link: "https://github.com/JoaoVictorDevMeta/Proj-Matematica-6-ano-2023",
   },
+  {
+    id: 2,
+    title: "Site da Monitoria",
+    description:
+      "Este projeto foi realizado com o intuito de facilitar a comunicação entre os monitores e os alunos. O site foi desenvolvido com HTML, CSS e JavaScript.",
+    link: "https://github.com/JoaoVictorDevMeta/Monitoria-LP"
+  }
 ];
